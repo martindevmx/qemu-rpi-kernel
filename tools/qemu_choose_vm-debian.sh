a58c2e0ce29df077b87f6bb1aedd5b82a9f54153
martindevmx <martindev.mx@outlook.com>
2016-10-21 22:02:03 -0500

    qemu_choose_vm-debian.sh
    NOTE: please run this script as shell-root on Debian. Tested on Debian 8.2 Jessie amd64

--- tools/qemu_choose_vm-debian.sh
+++ tools/qemu_choose_vm-debian.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# NOTE: please run this script as shell-root on Debian
+# fork by @martindev_ - tested on Debian 8.2 amd64
+#
 # author: edgaroliveira.dev@gmail.com
 # purpose: change and run virtual machines with qemu
-# date: 11/09/2016
+# date: 11/09/2016 - fork 21/10/2016
 # version: 1.0
 # Font: http://dhruvvyas.com/blog/?p=49 -> automation of tasks in this post
 #---------------------------------------------------------------------------------
@@ -15,12 +18,26 @@
 # Download all images (in formatted .img) of you want run to this new empty folder
 # Run this script, following the instructions and be happy
 
-sudo apt-get update -y
-sudo apt-get install git -y
-sudo apt-get install gemu-system -y
+apt-get update -y
+apt-get install git -y
+apt-get install qemu-system -y
+
 git clone https://github.com/dhruvvyas90/qemu-rpi-kernel.git
 mv qemu-rpi-kernel/* .
-sudo rm -r qemu-rpi-kernel
+rm -r qemu-rpi-kernel
+
+## list of kernels and choose one
+echo "List of kernels availables:"
+array=($(ls [kernel-]*))
+for (( i=0; i<${#array[@]}; i++));
+do
+	echo $i - ${array[$i]}
+done
+echo "Please, choose one kernel:"
+read answer
+kernel_version="$(echo ${array[answer]})"
+echo "-${array[$answer]} selected"
+
 ## charge for an array the result of the command ls and list all value to can choose one of them
 array=($(ls *.img))
 for (( i=0; i<${#array[@]}; i++ ));
@@ -30,24 +47,25 @@ done
 echo "Choose one of the above, please!"
 read answer
 choose="$(echo ${array[$answer]} | grep -o formatted)"
+echo "${array[$answer]} selected"
 ## check if found pattern is "formatted"
-if [ $choose == formatted ]; 
+if [[ $choose == formatted ]]; 
 then
-	qemu-system-arm -kernel kernel-qemu-4.1.7-jessie -cpu arm1176 -m 256 -M versatilepb -serial stdio -append "root=/dev/sda2 rootfstype=ext4 rw" -hda ${array[$answer]} 
+	qemu-system-arm -kernel $kernel_version -cpu arm1176 -m 256 -M versatilepb -serial stdio -show-cursor -append "root=/dev/sda2 rootfstype=ext4 rw" -hda ${array[$answer]} 
 else
 	value="$(fdisk -l ${array[$answer]} | grep -o -i -P 'img2\s+[0-9]+[^0-9]' | sed -r -e 's/img2 +//g')"
 	multiple=512
 	reulstmultiply=$((value * multiple))
 	mkdir folder_mount
-	sudo mount -v -o offset=$reulstmultiply -t ext4 ${array[$answer]} folder_mount
+	mount -v -o offset=$reulstmultiply -t ext4 ${array[$answer]} folder_mount
 	## comment all lines
-	sudo sed -i 's/^/#/g' folder_mount/etc/ld.so.preload
+	sed -i 's/^/#/g' folder_mount/etc/ld.so.preload
 	## comment only lines with this certain standard
-	sudo sed -i '/dev\/mmcblk/ s?^?#?' folder_mount/etc/fstab
-	sudo umount folder_mount
+	sed -i '/dev\/mmcblk/ s?^?#?' folder_mount/etc/fstab
+	umount folder_mount
 	rmdir folder_mount
 	## change name current file (*.img) with this tag: "formatted"
 	mv ${array[$answer]} formatted_${array[$answer]}
 	## Execute qemu and be happy
-	qemu-system-arm -kernel kernel-qemu-4.1.7-jessie -cpu arm1176 -m 256 -M versatilepb -serial stdio -append "root=/dev/sda2 rootfstype=ext4 rw" -hda formatted_${array[$answer]} 
+	qemu-system-arm -kernel $kernel_version -cpu arm1176 -m 256 -M versatilepb -serial stdio -show-cursor -append "root=/dev/sda2 rootfstype=ext4 rw" -hda formatted_${array[$answer]} 
 fi
